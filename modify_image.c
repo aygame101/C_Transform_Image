3d7c74bda1f9dded3d5d6d56533b09ec7ff74062
aygame101 <aygame101@gmail.com>
2024-01-11 16:38:12 +0100

    add possibility in the menu

--- modify_image.c
+++ modify_image.c
@@ -6,7 +6,7 @@
 typedef struct {
     int width;
     int height;
-    unsigned char *data;
+    unsigned char* pixels;
 } GrayImage;
 
 typedef struct {
@@ -26,15 +26,42 @@ void welcome() {
     printf("             \\/          \\/            \\/     \\/ \n\n");
 }
 
-int choix() {
+// Detecter si le fichier est en ppm ou pgm et executer une fonction en conséquence (fonction en double, une pour pgm et une pour ppm).
+
+
+GrayImage mirrorImage(const GrayImage* image) {
+    GrayImage mirroredImage;
+    mirroredImage.width = image->width;
+    mirroredImage.height = image->height;
+    mirroredImage.pixels = (unsigned char*)malloc(image->width * image->height);
+
+    for (int i = 0; i < image->height; i++) {
+        for (int j = 0; j < image->width; j++) { 
+            mirroredImage.pixels[i * image->width + j] = image->pixels[i * image->width + (image->width - 1 - j)];
+        }
+    }
+
+    return mirroredImage;
+}
+
+void choix() {
     int choixUtilisateur;
+    choixUtilisateur = -1;
     
     printf("Que souhaitez vous faire ?\n\n");
     printf("1. Translation d'image\n");
     printf("2. Rotation d'image\n");
+    printf("3. Aggrandissement d'image\n");
+    printf("4. Contraste d'image\n");
+    printf("5. Luminosité d'image\n");
+    printf("6. Seuillage d'image\n");
+    printf("7. Flouter l'image\n");
+    printf("8. Pixeliser l'image\n");
+    printf("9. Passer en négatif\n");
+
     printf("0. Quitter\n\n");
     printf("Entrez votre choix : ");
-    scanf("%d", &choixUtilisateur);
+    scanf(" %d", &choixUtilisateur);
 
     switch (choixUtilisateur) {
         case 1:
@@ -44,14 +71,12 @@ int choix() {
             // Appel fonction rotation
             break;
         case 0:
-            printf("Aurevoir.\n");
+            printf("\n\n\nAurevoir.\n\n\n");
             break;
         default:
-            printf("Choix invalide. Veuillez entrer 0, 1 ou 2.\n");
+            printf("\n\n\nChoix invalide. Veuillez entrer 0, 1 ou 2.\n\n\n");
             break;
     }
-
-    return choixUtilisateur;
 }
 
 int main() {
