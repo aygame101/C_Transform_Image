dc477b79d3a3dec3946e908129e4d2c1e4fc5764
aygame101 <aygame101@gmail.com>
2024-01-23 18:48:22 +0100

    blur image

--- modify_image.c
+++ modify_image.c
@@ -337,6 +337,55 @@ GrayImage thresholdImage(const GrayImage* image, unsigned char threshold) {
     return thresholdedImage;
 }
 
+// Fonction pour appliquer un flou à l'image en fonction de la valeur de flou
+GrayImage applyBlur(const GrayImage* image, int blurAmount) {
+    int width = image->width;
+    int height = image->height;
+
+    GrayImage blurredImage;
+    blurredImage.width = width;
+    blurredImage.height = height;
+    blurredImage.pixels = (unsigned char*)malloc(width * height);
+
+    if (blurredImage.pixels == NULL) {
+        perror("Erreur d'allocation de mémoire");
+        exit(1);
+    }
+
+    // Créer un noyau de flou (ici, un noyau de moyenne)
+    double kernel[3][3] = {{1.0 / 9, 1.0 / 9, 1.0 / 9},
+                           {1.0 / 9, 1.0 / 9, 1.0 / 9},
+                           {1.0 / 9, 1.0 / 9, 1.0 / 9}};
+
+    // Appliquer le flou en fonction de la valeur de flou
+    for (int amount = 0; amount < blurAmount; amount++) {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                double sum = 0.0;
+
+                for (int i = -1; i <= 1; i++) {
+                    for (int j = -1; j <= 1; j++) {
+                        int newX = x + j;
+                        int newY = y + i;
+
+                        // Assurer que nous sommes à l'intérieur de l'image
+                        if (newX >= 0 && newX < width && newY >= 0 && newY < height) {
+                            sum += kernel[i + 1][j + 1] * image->pixels[newY * width + newX];
+                        }
+                    }
+                }
+
+                blurredImage.pixels[y * width + x] = (unsigned char)sum;
+            }
+        }
+    }
+
+    return blurredImage;
+}
+
+
+
+
 
 void choix() {
     int choixUtilisateur;
@@ -519,6 +568,26 @@ void choix() {
         }
         case 8: {
             char filename[256] = "";
+            int blurAmount = 0;
+
+            printf("Entrez le nom du fichier : ");
+            scanf("%s", filename);
+
+            printf("Entrez le rayon du flou (plus le rayon est grand, plus le flou est important) : ");
+            scanf("%d", &blurAmount);
+
+            originalImage = loadPGM(filename);
+            adjustedImage = applyBlur(&originalImage, blurAmount);
+
+            strcat(filename, "_BLUR.pgm");
+            savePGM(filename, &adjustedImage);
+
+            free(originalImage.pixels);
+            free(adjustedImage.pixels);
+
+            printf("\n\n\nL'image a bien été traitée.\n\n\n");
+
+            break;
         }
         case 0: {
             printf("\n\n\nAurevoir.\n\n\n");
